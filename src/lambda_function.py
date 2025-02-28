bac84c34a9813fa822f5eb198e6a1d4f498cf5f4
Favour Onyeneke <120392002+onlyfave@users.noreply.github.com>
2025-02-28 12:12:30 +0100

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,1 +1,25 @@
+import json
+import os
+import boto3
+import requests
+
+# Load environment variables
+SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
+
+def lambda_handler(event, context):
+    message = event['Records'][0]['Sns']['Message']
+    
+    payload = {
+        "text": f":rotating_light: *OpsAlert Notification* :rotating_light:\n{message}"
+    }
+
+    headers = {'Content-Type': 'application/json'}
+    
+    # Send message to Slack
+    response = requests.post(SLACK_WEBHOOK_URL, data=json.dumps(payload), headers=headers)
+
+    return {
+        "statusCode": response.status_code,
+        "body": json.dumps("Alert sent to Slack successfully")
+    }
 
